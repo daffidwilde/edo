438cfc1db27f73567d3f2133da14ddc6a05695d5
Henry Wilde <henrydavidwilde@gmail.com>
2018-08-02 13:13:43 +0100

    Base distribution class (#27)
    * "Blackerise" config and setup scripts.
    
    * Add Distribution class and write its test.

--- src/genetic_data/pdfs.py
+++ src/genetic_data/pdfs.py
@@ -3,7 +3,16 @@
 import numpy as np
 
 
-class Gamma:
+class Distribution(object):
+    """ A base class for the distributions below and those defined by users. """
+
+    def sample(self):
+        """ Raise a NotImplementedError by default. """
+
+        raise NotImplementedError("You must define a sample method.")
+
+
+class Gamma(Distribution):
     """ Continuous column pdf given by the gamma distribution. """
 
     alpha_limits = [0, 10]
@@ -14,6 +23,10 @@ class Gamma:
         self.alpha = np.random.uniform(*self.alpha_limits)
         self.theta = np.random.uniform(*self.theta_limits)
 
+    def __repr__(self):
+
+        return f"Gamma(alpha={self.alpha:.2f}, theta={self.theta:.2f})"
+
     def sample(self, nrows):
         """ Take a sample of size `nrows` from the gamma distribution with
         shape and scale parameters given by `alpha` and `theta` respectively.
@@ -22,7 +35,7 @@ class Gamma:
         return np.random.gamma(shape=self.alpha, scale=self.theta, size=nrows)
 
 
-class Normal:
+class Normal(Distribution):
     """ Continuous column pdf given by the normal distribution. """
 
     mean_limits = [-10, 10]
@@ -34,6 +47,7 @@ class Normal:
         self.std = np.random.uniform(*self.std_limits)
 
     def __repr__(self):
+
         return f"Normal(mean={self.mean:.2f}, std={self.std:.2f})"
 
     def sample(self, nrows):
@@ -43,7 +57,7 @@ class Normal:
         return np.random.normal(loc=self.mean, scale=self.std, size=nrows)
 
 
-class Poisson:
+class Poisson(Distribution):
     """ Discrete column pdf given by the Poisson distribution. """
 
     lam_limits = [0, 10]
