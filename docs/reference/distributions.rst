2f7ea96f2669d72b1ceed83be08b25c7df3e9fd9
Henry Wilde <henrydavidwilde@gmail.com>
2018-09-03 08:45:38 +0100

    Docs reference (#56)
    * Preserve dtypes, common __repr__ method (#51)
    
    * Revert "Add pdf classes"
    
    * Merging into master
    
    * Change name in README. (#42)
    
    * Update README.rst
    
    * Update project name in `setup.py`
    
    * Include dist in gitignore.
    
    * Move __repr__ to base Distribution class.
    
    * Add dtype as class attribute; simplify pdf tests.
    
    * Correct dtype of column when filling in NaNs.
    
    * Modularise operator tests (#52)
    
    * Revert "Add pdf classes"
    
    * Merging into master???
    
    * Change name in README. (#42)
    
    * Update README.rst
    
    * Update project name in `setup.py`
    
    * Include dist in gitignore.
    
    * Move __repr__ to base Distribution class.
    
    * Add dtype as class attribute; simplify pdf tests.
    
    * Correct dtype of column when filling in NaNs.
    
    * Break operator tests up; rename operator scripts.
    
    * Blacken.
    
    * Modularise pdf classes.
    
    * Auto-gen the pdf docs.

--- docs/reference/distributions.rst
+++ docs/reference/distributions.rst
@@ -1,18 +1,28 @@
 .. _dists:
 
 Implemented distributions
--------------------------
+=========================
 
-The following distributions are currently implemented in Edo:
+Below are all the distributions currently implemented in Edo, each of which
+inherit from the :code:`Distribution` base class:
+
+.. automodule:: edo.pdfs.base
+   :members:
+
+.. note::
+   All the implemented distributions can be accessed directly from
+   :code:`edo.pdfs`.
 
 Continuous
-++++++++++
+----------
 
-- Normal
-- Gamma
+.. automodule:: edo.pdfs.continuous
+   :members:
+   :show-inheritance:
 
 Discrete
-++++++++
+--------
 
-- Poisson
-- Binary
+.. automodule:: edo.pdfs.discrete
+   :members:
+   :show-inheritance:
---
+++ src/edo/pdfs/discrete.py
@@ -0,0 +1,68 @@
+""" All currently implemented, discrete distribution classes. """
+
+import numpy as np
+
+from .base import Distribution
+
+class Bernoulli(Distribution):
+    """ Discrete column class given by the Bernoulli distribution. That is, a
+    binomial distribution with exactly one trial.
+
+    Attributes
+    ----------
+    name : str
+        Name of the distribution, :code:`"Bernoulli"`.
+    dtype : str
+        Preferred datatype of distribution, :code:`"int"`.
+    prob_limits : list
+        Limits on the success probability parameter, :code:`prob`. Defaults to
+        :code:`[0, 1]`.
+    prob : float
+        The success probability, sampled from :code:`prob_limits`. `Instance
+        attribute.`
+    """
+
+    name = "Bernoulli"
+    dtype = "int"
+    prob_limits = [0, 1]
+
+    def __init__(self):
+
+        self.prob = np.random.uniform(*self.prob_limits)
+
+    def sample(self, nrows):
+        """ Take a sample of size :code:`nrows` from the Bernoulli distribution
+        with success probability :code:`prob`. """
+
+        return np.random.binomial(n=1, p=self.prob, size=nrows)
+
+
+class Poisson(Distribution):
+    """ Discrete column class given by the Poisson distribution.
+
+    Attributes
+    ----------
+    name : str
+        Name of distribution, :code:`"Poisson"`.
+    dtype : str
+        Preferred datatype of distribution, :code:`"int"`.
+    lam_limits : list
+        Limits on the rate parameter, :code:`lam`. Defaults to :code:`[0, 10]`.
+    lam : float
+        The rate parameter, sampled from :code:`lam_limits`. `Instance
+        attribute.`
+    """
+
+    name = "Poisson"
+    dtype = "int"
+    lam_limits = [0, 10]
+
+    def __init__(self):
+
+        self.lam = np.random.uniform(*self.lam_limits)
+
+    def sample(self, nrows):
+        """ Take a sample of size :code:`nrows` from the Poisson distribution
+        with rate parameter :code:`lam`. """
+
+        return np.random.poisson(lam=self.lam, size=nrows)
