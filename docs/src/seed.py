fa82f776018abe4eccf8a681488d4d2d68253fdf
Henry Wilde <henrydavidwilde@gmail.com>
2018-09-17 10:52:38 +0100

    Blacken documentation source code.

--- docs/src/seed.py
+++ docs/src/seed.py
@@ -7,9 +7,11 @@ from matplotlib.patches import Patch
 import edo
 from edo.pdfs import Normal
 
+
 def x_squared(df):
     """ Squared-value fitness. """
     return df.iloc[0, 0] ** 2
+
 
 def main():
     """ Run the GA with two seeds to show they produce different results. """
@@ -21,7 +23,7 @@ def main():
         col_limits=[1, 1],
         pdfs=[Normal],
         max_iter=5,
-        seed=0
+        seed=0,
     )
 
     new_pop, new_fit, new_all_pops, new_all_fits = edo.run_algorithm(
@@ -31,7 +33,7 @@ def main():
         col_limits=[1, 1],
         pdfs=[Normal],
         max_iter=5,
-        seed=1
+        seed=1,
     )
 
     fig, ax = plt.subplots(1, figsize=(32, 12), dpi=300)
@@ -41,40 +43,38 @@ def main():
     shift = .5 * width + epsilon
 
     old = ax.boxplot(
-        all_fits,
-        positions=positions-shift,
-        widths=width,
-        patch_artist=True
+        all_fits, positions=positions - shift, widths=width, patch_artist=True
     )
 
     new = ax.boxplot(
         new_all_fits,
-        positions=positions+shift,
+        positions=positions + shift,
         widths=width,
-        patch_artist=True
+        patch_artist=True,
     )
 
     ax.set_xticks(positions)
     ax.set_xticklabels(positions)
-    ax.set_yscale('log')
-    ax.set_xlabel('Epoch', fontsize=24)
-    ax.set_ylabel(r'$\log (f(x))$', fontsize=24)
+    ax.set_yscale("log")
+    ax.set_xlabel("Epoch", fontsize=24)
+    ax.set_ylabel(r"$\log (f(x))$", fontsize=24)
 
-    for plot, colour in zip([old, new], ['lightblue', 'lightpink']):
-        for patch in plot['boxes']:
+    for plot, colour in zip([old, new], ["lightblue", "lightpink"]):
+        for patch in plot["boxes"]:
             patch.set_facecolor(colour)
 
     for label in ax.get_xticklabels() + ax.get_yticklabels():
         label.set_fontsize(20)
 
     ax.legend(
-        handles=[Patch(color='lightblue'), Patch(color='lightpink')],
-        labels=['Seed 0', 'Seed 1'],
-        fontsize=24
+        handles=[Patch(color="lightblue"), Patch(color="lightpink")],
+        labels=["Seed 0", "Seed 1"],
+        fontsize=24,
     )
 
     plt.tight_layout()
-    plt.savefig('../_static/seed.svg', format='svg', transparent=True)
+    plt.savefig("../_static/seed.svg", format="svg", transparent=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
