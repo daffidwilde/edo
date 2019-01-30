cb0f193be9445e1620c770850c4d7df2325a06a4
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-30 23:54:04 +0000

    Add edo to path and mock modules. (#85)

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+import mock
+
+
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+sys.path.insert(0, os.path.abspath('../src/edo/'))
+
+import edo
 
 
 # -- General configuration ------------------------------------------------
