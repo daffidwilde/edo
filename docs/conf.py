419e1f6699bc8143487a8e4f26e43dcf514f6fec
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-30 23:59:30 +0000

    Sphinx autodoc (#86)
    * Add edo to path and mock modules.
    
    * Alter src path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
-sys.path.insert(0, os.path.abspath('../src/edo/'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 import edo
 
