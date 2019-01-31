24ea8dd598f0185df06ee5b0847436d969814c27
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-31 00:04:30 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
 import edo
